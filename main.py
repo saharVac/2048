5249241776117d9564ca87433c46ecbc89b072c5
saharv-av <vcsahar@gmail.com>
2021-02-20 17:32:12 -0500

    # generate random 2 or 4 in tile at random row and column

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
+from random import choice
 
+# generate random 2 or 4 in tile at random row and column
+
+tile = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
+
+new_num = choice([2, 4])
+new_row = choice([0, 1, 2, 3])
+new_col = choice([0, 1, 2, 3])
+tile[new_row][new_col] = new_num
 
 print("|         |         |         |         |")
-print("|         |         |         |         |")
-print("|         |         |         |         |")
-print("-----------------------------------------")
-print("|         |         |         |         |")
-print("|         |         |         |         |")
+print("|    " + str(tile[0][0]) + "    |    " + str(tile[0][1]) + "    |    " + str(tile[0][2]) + "    |    " + str(tile[0][3]) + "    |")
 print("|         |         |         |         |")
 print("-----------------------------------------")
 print("|         |         |         |         |")
-print("|         |         |         |         |")
+print("|    " + str(tile[1][0]) + "    |    " + str(tile[1][1]) + "    |    " + str(tile[1][2]) + "    |    " + str(tile[1][3]) + "    |")
 print("|         |         |         |         |")
 print("-----------------------------------------")
 print("|         |         |         |         |")
+print("|    " + str(tile[2][0]) + "    |    " + str(tile[2][1]) + "    |    " + str(tile[2][2]) + "    |    " + str(tile[2][3]) + "    |")
 print("|         |         |         |         |")
+print("-----------------------------------------")
+print("|         |         |         |         |")
+print("|    " + str(tile[3][0]) + "    |    " + str(tile[3][1]) + "    |    " + str(tile[3][2]) + "    |    " + str(tile[3][3]) + "    |")
 print("|         |         |         |         |")
