92a881d2e6a61927b9efa21019b0d29ea7b51001
saharv-av <vcsahar@gmail.com>
2021-02-20 17:51:11 -0500

    not showing tiles with value 0

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from random import choice
 
-# generate random 2 or 4 in tile at random row and column
+# don't show tiles with value 0
 
-tile = [
+tiles = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -12,20 +12,14 @@ tile = [
 new_num = choice([2, 4])
 new_row = choice([0, 1, 2, 3])
 new_col = choice([0, 1, 2, 3])
-tile[new_row][new_col] = new_num
+tiles[new_row][new_col] = new_num
 
-print("|         |         |         |         |")
-print("|    " + str(tile[0][0]) + "    |    " + str(tile[0][1]) + "    |    " + str(tile[0][2]) + "    |    " + str(tile[0][3]) + "    |")
-print("|         |         |         |         |")
-print("-----------------------------------------")
-print("|         |         |         |         |")
-print("|    " + str(tile[1][0]) + "    |    " + str(tile[1][1]) + "    |    " + str(tile[1][2]) + "    |    " + str(tile[1][3]) + "    |")
-print("|         |         |         |         |")
-print("-----------------------------------------")
-print("|         |         |         |         |")
-print("|    " + str(tile[2][0]) + "    |    " + str(tile[2][1]) + "    |    " + str(tile[2][2]) + "    |    " + str(tile[2][3]) + "    |")
-print("|         |         |         |         |")
-print("-----------------------------------------")
-print("|         |         |         |         |")
-print("|    " + str(tile[3][0]) + "    |    " + str(tile[3][1]) + "    |    " + str(tile[3][2]) + "    |    " + str(tile[3][3]) + "    |")
-print("|         |         |         |         |")
+for row in tiles:
+    for line in range(5):
+        if line == 2:
+            print("|     " + (str(row[0]) if row[0] else " ") + "     |     " + (
+                str(row[1]) if row[1] else " ") + "     |     " + (str(
+                row[2]) if row[2] else " ") + "     |     " + (str(row[3]) if row[3] else " ") + "     |")
+        else:
+            print("|           |           |           |           |")
+    print("-------------------------------------------------")
