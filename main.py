88e894748709149ed21bdcf152b7a98a110946ce
saharv-av <vcsahar@gmail.com>
2021-02-24 22:09:11 -0500

    redesigned board and formatted upward movement

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
 from random import choice
 
-# don't show tiles with value 0
+# move the tiles
+from pip._vendor.distlib.compat import raw_input
 
 tiles = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
+    [2, 0, 0, 0],
+    [2, 0, 0, 0],
+    [2, 0, 0, 0],
+    [2, 0, 0, 0]
 ]
 
-new_num = choice([2, 4])
-new_row = choice([0, 1, 2, 3])
-new_col = choice([0, 1, 2, 3])
-tiles[new_row][new_col] = new_num
 
-for row in tiles:
-    for line in range(5):
-        if line == 2:
-            print("|     " + (str(row[0]) if row[0] else " ") + "     |     " + (
-                str(row[1]) if row[1] else " ") + "     |     " + (str(
-                row[2]) if row[2] else " ") + "     |     " + (str(row[3]) if row[3] else " ") + "     |")
-        else:
-            print("|           |           |           |           |")
-    print("-------------------------------------------------")
+def add_tile():
+    new_num = choice([2, 4])
+    while True:
+        new_row = choice([0, 1, 2, 3])
+        new_col = choice([0, 1, 2, 3])
+        if tiles[new_row][new_col] == 0:
+            tiles[new_row][new_col] = new_num
+            break
+
+
+def print_board():
+    for row in tiles:
+        print(row)
+
+
+def move(direction):
+    if direction == "up":
+        for col in range(4):
+            # if first in column equals second in column
+            if tiles[0][col] == tiles[1][col]:
+                tiles[0][col] = tiles[0][col] * 2
+                tiles[3][col] = 0
+                # if third in column equals fourth in column
+                if tiles[2][col] == tiles[3][col]:
+                    tiles[1][col] = tiles[2][col] * 2
+                    tiles[2][col] = 0
+                else:
+                    tiles[1][col] = tiles[2][col]
+                    tiles[2][col] = tiles[3][col]
+
+
+def game():
+    while True:
+        print_board()
+        next_move = input("Next Move:")
+        move(next_move)
+
+
+game()
