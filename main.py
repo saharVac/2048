44d1b802b389975e95b3411f096f045bc594c5da
saharv-av <vcsahar@gmail.com>
2021-03-07 22:10:58 -0500

    Accounting for more scenarios, Adjusting algorithm

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from random import choice
 from pip._vendor.distlib.compat import raw_input
 
 tiles = [
-    [2, 0, 0, 0],
-    [2, 0, 0, 0],
-    [2, 0, 0, 0],
+    [4, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
     [2, 0, 0, 0]
 ]
 
@@ -29,26 +29,81 @@ def print_board():
 def move(direction):
     if direction == "up":
         for col in range(4):
-            print("col", col, [tiles[0][col], tiles[1][col], tiles[2][col], tiles[3][col]])
-            # if first in column equals second in column
-            if tiles[0][col] == tiles[1][col]:
-                tiles[0][col] = tiles[0][col] * 2
-                tiles[3][col] = 0
-                print("third in column equals fourth in column", tiles[2][col] == tiles[3][col], " third", tiles[2][col], " fourth", tiles[3][col])
-                # if third in column equals fourth in column
-                if tiles[2][col] == tiles[3][col]:
-                    print("EQUALS", tiles[2][col])
-                    tiles[1][col] = tiles[2][col] * 2
-                    tiles[2][col] = 0
+            # tiles[0][col] = tiles[0][col] == tiles[1][col] ? tiles[0][col] * 2 :
+            # if first in column isn't 0 [nonzero, ..., ..., ...]
+            if tiles[0][col] != 0:
+                # if first in column equals second in column [equals, equals, ..., ...]
+                if tiles[0][col] == tiles[1][col]:
+                    tiles[0][col] = tiles[0][col] * 2
+                    # if third in column equals fourth in column [equals1, equals1, equals2, equals2]
+                    if tiles[2][col] == tiles[3][col]:
+                        tiles[1][col] = tiles[2][col] * 2
+                        tiles[2][col] = 0
+                    # if third in column doesn't equal fourth in column [equals1, equals1, nonequal, nonequal]
+                    else:
+                        # if third in column is nonzero 0 [equals1, equals1, nonzero , nonequal]
+                        if tiles[2][col] != 0:
+                            tiles[1][col] = tiles[2][col]
+                            tiles[2][col] = tiles[3][col]
+                        # if third in column is  0 [equals1, equals1, 0 , nonzero]
+                        else:
+                            tiles[1][col] = tiles[3][col]
+                            tiles[2][col] = 0
+                    tiles[3][col] = 0
+                # if first in column doesn't equal second in column
                 else:
-                    tiles[1][col] = tiles[2][col]
-                    tiles[2][col] = tiles[3][col]
+                    # if second in column isn't 0 [nonzero nonequal, nonzero nonequal, ..., ...]
+                    if tiles[1][col] != 0:
+                        if tiles[1][col] == tiles[2][col]:
+                            tiles[1][col] = tiles[2][col] * 2
+                            tiles[2][col] = tiles[3][col]
+                            tiles[3][col] = 0
+                        else:
+                            tiles[1][col] = tiles[2][col]
+                            tiles[2][col] = tiles[3][col]
+                    # if second in column is 0 [nonzero, 0, ..., ...]
+                    else:
+                        # if third in column is 0 [nonzero, 0, 0, ...]
+                        if tiles[1][col] == tiles[2][col]:
+
+            # if first in column is 0 [0, ..., ..., ...]
+            else:
+                # if first in column equals second in column [0, 0, ..., ...]
+                if tiles[0][col] == tiles[1][col]:
+                    # if second in column equals third in column [0, 0, 0, ...]
+                    if tiles[1][col] == tiles[2][col]:
+                        tiles[0][col] = tiles[3][col]
+                    # if second in column doesn't equal third in column [0, 0, nonzero, ...]
+                    else:
+                        tiles[0][col] = tiles[2][col]
+                        tiles[1][col] = tiles[3][col]
+                    tiles[2][col] = 0
+                    tiles[3][col] = 0
+                # if first in column doesn't equal second in column [0, nonzero, ..., ...]
+                else:
+                    # if second in column equals third in column [0, equal, equal, ...]
+                    if tiles[1][col] == tiles[2][col]:
+                        tiles[0][col] = tiles[1][col] * 2
+                        tiles[1][col] = tiles[3][col]
+                    # if second in column doesn't equal third in column [0, nonequal, nonequal, ...]
+                    else:
+                        tiles[0][col] = tiles[1][col]
+                        # if third in column equals fourth in column [0, ..., equal, equal]
+                        if tiles[2][col] == tiles[3][col]:
+                            tiles[1][col] = tiles[2][col] * 2
+                            tiles[2][col] = 0
+                        # if third in column doesn't equal fourth in column [0, ..., nonequal, nonequal]
+                        else:
+                            tiles[1][col] = tiles[2][col]
+                            tiles[2][col] = tiles[3][col]
+                    tiles[3][col] = 0
 
 
 def game():
     while True:
+        add_tile()
         print_board()
-        next_move = input("Next Move:")
+        next_move = input("Next Move: ")
         move(next_move)
 
 
